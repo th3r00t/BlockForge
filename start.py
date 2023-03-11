eaf421646d65ada1bfa961d138e76de118c7f317
th3root <admin@mylt.dev>
2023-03-11 11:29:54 -0500

    refactored into seperate build function.

--- start.py
+++ start.py
@@ -8,40 +8,43 @@ def main():
         with open(target_file, "w") as target:
             for line in source:
                 _l = line.split("/")
-                subtypeid = _l[1]
-                try:
-                    count = _l[2]
-                except IndexError:
-                    count = 10
-                render = f"""
-        new BlockDef()
-        {{
-            BlockName = "CubeBlock/{subtypeid}",
-                BlockActions = new[]
-                {{
-                    new BlockAction
-                    {{
-                        Action = ReplaceComponent,
-                        Component = "MyObjectBuilder_Component/TitaniumNanocomposite",
-                        Index = 1,
-                        Count = {count}
-                    }},
-                    new BlockAction
-                    {{
-                        Action = ChangeComponentDeconstructId,
-                        Component = "MyObjectBuilder_Ore/Scrap",
-                        Index = 1
-                    }},
-                    new BlockAction
-                    {{
-                        Action = ChangeCriticalComponentIndex,
-                        Index = 1
-                    }},
-                }}
-        }},
-        """
+                render: str = builder(_l)
                 target.write(render)
                 print(render)
+
+def builder(line: list) -> str:
+    subtypeid = line[1]
+    try:
+        count = line[2]
+    except IndexError:
+        count = 10
+    return f"""
+    new BlockDef()
+    {{
+        BlockName = "CubeBlock/{subtypeid}",
+            BlockActions = new[]
+            {{
+                new BlockAction
+                {{
+                    Action = ReplaceComponent,
+                    Component = "MyObjectBuilder_Component/TitaniumNanocomposite",
+                    Index = 1,
+                    Count = {count}
+                }},
+                new BlockAction
+                {{
+                    Action = ChangeComponentDeconstructId,
+                    Component = "MyObjectBuilder_Ore/Scrap",
+                    Index = 1
+                }},
+                new BlockAction
+                {{
+                    Action = ChangeCriticalComponentIndex,
+                    Index = 1
+                }},
+            }}
+    }},
+    """
 
 if __name__ == "__main__":
     main()
