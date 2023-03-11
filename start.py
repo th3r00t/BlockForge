db210cfd18805ae18f77d3f4ea53ed713c9f5f84
th3root <admin@mylt.dev>
2023-03-11 11:20:48 -0500

    Initial upload.

---
+++ start.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+source_file = "cubeblocklist.txt"
+target_file = "target.cs"
+
+def main():
+    with open(source_file, "r") as source:
+        with open(target_file, "w") as target:
+            for line in source:
+                _l = line.split("/")
+                subtypeid = _l[1]
+                count = _l[2]
+                breakpoint()
+                render = f"""
+        new BlockDef()
+        {{
+            BlockName = "CubeBlock/{subtypeid}",
+                BlockActions = new[]
+                {{
+                    new BlockAction
+                    {{
+                        Action = ReplaceComponent,
+                        Component = "MyObjectBuilder_Component/TitaniumNanocomposite",
+                        Index = 1,
+                        Count = {count}
+                    }},
+                    new BlockAction
+                    {{
+                        Action = ChangeComponentDeconstructId,
+                        Component = "MyObjectBuilder_Ore/Scrap",
+                        Index = 1
+                    }},
+                    new BlockAction
+                    {{
+                        Action = ChangeCriticalComponentIndex,
+                        Index = 1
+                    }},
+                }}
+        }},
+        """
+                target.write(render)
+                print(render)
+
+if __name__ == "__main__":
+    main()
